7068632c5761f882ad923dde4032599e618d5d80
kaiijuu7 <kailani.calub@gmail.com>
2026-03-29 00:48:52 -0400

    Home page changes
    - color
    - icons
    - text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Screen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Tab</Text>
+      <Text style={styles.text}>Trending Movie Locations</Text>
     </View>
   );
 }
@@ -16,7 +16,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#0F0A18', // A dark background similar to your Figma
   },
   text: {
-    color: 'white',
-    fontSize: 20,
+    flex: 1,
+    color: '#d9a3fb',
+    fontSize: 23,
+    fontWeight: 'bold',
+    justifyContent: 'flex-start',
+    right: 40,
+    padding: 20,
   },
 });
