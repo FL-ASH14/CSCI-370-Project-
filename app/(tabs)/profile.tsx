82f498687d9930957fdd9d985d0b45f3ce8b4bee
kaiijuu7 <kailani.calub@gmail.com>
2026-04-10 19:20:09 -0400

    Profile
    - headers

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,7 +3,8 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 export default function Screen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.headerText}>Jane Doe</Text>
+      <Text style={styles.userText}>Jane Doe</Text>
+      <Text style={styles.subUserText}>@janedoe</Text>
       <View style={styles.avatar}>
         <Image
           style={styles.avatar}
@@ -12,6 +13,10 @@ export default function Screen() {
           }}
         />
       </View>
+
+      <Text style={styles.headerText}>Favorite Movies</Text>
+      <Text style={styles.headerText}>Favorite Shows</Text>
+      <Text style={styles.headerText}>Saved Locations</Text>
     </View>
   );
 }
@@ -24,10 +29,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#0F0A18', // A dark background similar to your Figma
     paddingTop: 15,
   },
-  headerText: {
+  userText: {
     color: '#FF69B4',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  subUserText: {
+    color: '#a26a86',
+    fontSize: 17,
+  },
+  headerText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignSelf: 'flex-start',
+    paddingLeft: 20,
+    padding: 30,
+    marginBottom: 90
   },
   avatar: {
     height: 120,
