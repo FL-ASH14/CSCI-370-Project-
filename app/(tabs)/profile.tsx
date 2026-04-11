85ad53d54893546bc509aad264a698bd46343b3a
kaiijuu7 <kailani.calub@gmail.com>
2026-04-10 23:35:15 -0400

    Update profile
    - implementing user favorites

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView} from 'react-native';
+
+const favShows = [
+  { id: 'f1', imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_.jpg' },
+  { id: 'f2', imageUrl: 'https://mythicwall.com/cdn/shop/files/StrangerThings_1024x1024.jpg?v=1762444885' },
+  { id: 'f3', imageUrl: 'https://resizing.flixster.com/Lzq-nUwBS4DCJDOoSpOVTsCdJ5E=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vUlRUVjIwMTY1MC53ZWJw' }
+]
 
 export default function Screen() {
   return (
@@ -14,9 +20,23 @@ export default function Screen() {
         />
       </View>
 
+      // fav movies
       <Text style={styles.headerText}>Favorite Movies</Text>
+      
+
+      // fav shows
       <Text style={styles.headerText}>Favorite Shows</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favShows.map((show) => (
+          <View key={show.id} style={styles.card}>
+            <Image style={styles.image} source={{ uri: show.imageUrl }}/>
+          </View>
+        ))}
+      </ScrollView>
+
+      // saved locations
       <Text style={styles.headerText}>Saved Locations</Text>
+      
     </View>
   );
 }
@@ -52,5 +72,15 @@ const styles = StyleSheet.create({
     width: 120,
     borderRadius: 60,
     borderWidth: 5,
+  },
+  card: {
+    width: 120,
+    height: 180,
+    borderRadius: 10,
+    marginRight: 15,
+  },
+  image: {
+    width: '100%',
+    height: '100%'
   }
 });
